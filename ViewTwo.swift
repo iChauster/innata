6419b0148ca7792953b5684c37f93032f1ffc92c
Ivan Chau <ivanmchau@gmail.com>
2017-01-21 05:53:03 -0500

    ayy

---
+++ ViewTwo.swift
@@ -0,0 +1,154 @@
+//
+//  ViewTwo.swift
+//  Innata
+//
+//  Created by Ivan Chau on 1/20/17.
+//  Copyright © 2017 Innata. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewTwo: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var cam: UIView!
+    var capsesh : AVCaptureSession?
+    
+    
+    var imgResult : AVCaptureStillImageOutput?
+    var prelay : AVCaptureVideoPreviewLayer?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var i = UIImage(imageLiteralResourceName: "ReceiptSwiss.jpg")
+        let scaledImage = self.scaleImage(image: i, maxDimension: 200)
+        print(self.recog(image: scaledImage))
+        //when it captures
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func  viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        prelay?.frame = cam.bounds
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        capsesh = AVCaptureSession()
+        capsesh?.sessionPreset = AVCaptureSessionPresetHigh
+        
+        let camcord = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do{
+            let inputMeth = try AVCaptureDeviceInput(device: camcord)
+            if (capsesh?.canAddInput(inputMeth))!{
+                capsesh?.addInput(inputMeth)
+                    imgResult = AVCaptureStillImageOutput()
+                    imgResult?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+                if (capsesh?.canAddOutput(imgResult))!{
+                    capsesh?.addOutput(imgResult)
+                    
+                    prelay = AVCaptureVideoPreviewLayer(session: capsesh)
+                    
+                    prelay?.videoGravity = AVLayerVideoGravityResizeAspect
+                    
+                    prelay?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
+                    
+                    
+                    //camview
+                    
+                    cam.layer.addSublayer(prelay!)
+                    capsesh?.startRunning()
+                }
+            }
+        }catch let error as NSError{
+            print(error)
+        }
+    }
+
+    @IBOutlet var imgv: UIImageView!
+    func photoShot(){
+        if let videoConnection = imgResult?.connection(withMediaType: AVMediaTypeVideo){
+                videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+            imgResult?.captureStillImageAsynchronously(from: videoConnection, completionHandler: {(sampleBuffer, error) in
+            
+                if sampleBuffer != nil{
+                    let imgd = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dp = CGDataProvider(data : imgd as! CFData)
+                    let imgref = CGImage(jpegDataProviderSource: dp!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+                    DispatchQueue.main.async(execute: {
+                        let img = UIImage(cgImage: imgref!, scale: 1.0, orientation: UIImageOrientation.right)
+                        let scaledImage = self.scaleImage(image: img, maxDimension: 640)
+                        print(self.recog(image: scaledImage))
+                        self.imgv.image = img
+                        self.imgv.isHidden = false
+                        self.view.layer.zPosition = 0
+                    
+                    });
+                    
+                    
+                }})
+            
+        }
+    }
+    var fototook = Bool()
+    func anothaOne(){
+        if fototook == true{
+            imgv.isHidden = true
+            fototook = false
+        }else{
+            capsesh?.startRunning()
+            fototook = true
+            photoShot()
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        anothaOne()
+    }
+    func scaleImage(image: UIImage, maxDimension: CGFloat) -> UIImage {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        var scaleFactor: CGFloat
+        
+        if image.size.width > image.size.height {
+            scaleFactor = image.size.height / image.size.width
+            scaledSize.width = maxDimension
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            scaleFactor = image.size.width / image.size.height
+            scaledSize.height = maxDimension
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        
+        image.draw(in: CGRect(x:0, y:0, width:scaledSize.width, height:scaledSize.height))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage!
+    }
+    
+    func recog(image:UIImage) -> String{
+        let tesseract = G8Tesseract()
+        // 2
+        tesseract.language = "eng"
+        // 3
+        tesseract.engineMode = .tesseractCubeCombined
+        // 4
+        tesseract.pageSegmentationMode = .auto
+        // 5
+        tesseract.maximumRecognitionTime = 30.0
+        // 6
+        tesseract.image = image.g8_blackAndWhite()
+        tesseract.recognize()
+        // 7
+        return tesseract.recognizedText
+    }
+    
+}
